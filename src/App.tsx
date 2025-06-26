4bb87bffa306ba3ea0d8ac6247f055ef0b1979c8
Tapeline <yt.redstone.mail@gmail.com>
2025-06-27 00:57:55 +0500

    Remove dark theme, set adequate radius

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import {ThemeProvider} from "@/components/ui/theme-provider.tsx";
 
 function App() {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <Outlet/>
-        </ThemeProvider>
+        <Outlet/>
     )
 }
 
