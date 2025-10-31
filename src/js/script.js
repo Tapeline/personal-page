2ced4914d28ced9a880ef2acf0c8d1e8101904fd
Tapeline <mail@tapeline.dev>
2025-10-31 14:13:21 +0500

    Improve UI/UX and add an article

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,23 @@ $(document).ready(() => {
     //     console.log($(this));
     //     $(this).next("img").toggleClass("zoomed-media")
     // });
+    //$(".zoom-on-click img").each((i, elem) => {
+    //    const $elem = $(elem);
+    //    $elem.on("click", () => {
+    //        $elem.parent().addClass("zoomed-media")
+    //    })
+    //})
+    let isZoomed = false;
     $(".zoom-on-click").each((i, elem) => {
         const $elem = $(elem);
-        $elem.on("click", () => {
-            $elem.toggleClass("zoomed-media");
+        $elem.on("click", e => {
+            if (!isZoomed) {
+                $elem.addClass("zoomed-media");
+                isZoomed = true;
+            } else if (e.target.tagName !== "IMG") {
+                $elem.removeClass("zoomed-media")
+                isZoomed = false;
+            }
         })
     })
 })
